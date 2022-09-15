e367fe757d79d8f4bd79748a7457a1357e32678a
smrrrns <smrrrns@yandex.ru>
2022-09-15 17:37:38 +0300

    changed sort algorithm in IntSort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,11 +8,37 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+
+  public static void sort (int[] array, int beg, int end)
+  {
+    if (array.length == 0 | beg >= end)
+      return;
+    int mid = beg + (end - beg) / 2;
+    int flag = array[mid];
+
+    int i = beg, j = end;
+    while(i <= j)
+    {
+      while(array[i] < flag) i++;
+      while(array[j] > flag) j--;
+
+      if(i <= j)
+      {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }
+    }
+
+    if(beg < j) sort(array, beg, j);
+    if(end > i) sort(array, i, end);
+
   }
 
   public static void sort (List<Integer> list) {
     Collections.sort(list);
   }
+
 }
